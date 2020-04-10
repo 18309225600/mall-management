adbed3a27052f0434aa13d6a557782a7f4ef08fa
刘宏飞 <18309225600@163.com>
2020-04-10 22:34:04 +0800

    init

---
+++ src/test/java/com/lhf/mall/management/MallManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhf.mall.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
