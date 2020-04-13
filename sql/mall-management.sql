62c71301ede8079ca0fe93f8e2e776905383a1b7
刘宏飞 <18309225600@163.com>
2020-04-13 17:45:17 +0800

    sql

--- sql/mall-management.sql
+++ sql/mall-management.sql
@@ -1,34 +1,129 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : mall-management
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50721
+ Source Host           : localhost:3306
+ Source Schema         : mall-management
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : 65001
 
-Date: 2020-04-11 00:08:21
+ Date: 13/04/2020 17:44:57
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `mall_user`
+-- Table structure for mall_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `mall_customer`;
+CREATE TABLE `mall_customer`  (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户姓名',
+  `phone` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户电话',
+  `region` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '地区',
+  `region_id` bigint(20) NULL DEFAULT NULL COMMENT '地区ID',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '详细地址',
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '留言',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `deleted_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for mall_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `mall_goods`;
+CREATE TABLE `mall_goods`  (
+  `id` bigint(10) NOT NULL,
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '产品品名',
+  `url_suffix` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'url后缀',
+  `facebook_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'facebook像素编号',
+  `line_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'line像素编号',
+  `template_id` bigint(20) NULL DEFAULT NULL COMMENT '模板ID',
+  `template_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '模板名称',
+  `domain_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '域名',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `special_offer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '特价',
+  `original_offer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '原价',
+  `currency_symbol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '货币符号',
+  `sales_volume` int(10) NULL DEFAULT NULL COMMENT '销量',
+  `remaining_num` int(10) NULL DEFAULT NULL COMMENT '剩余件数',
+  `show_shop_match` tinyint(1) NULL DEFAULT NULL COMMENT '是否展示711店配 0否  1是',
+  `show_discount_code` tinyint(1) NULL DEFAULT NULL COMMENT '是否展示优惠码 0否 1是',
+  `default_selection_specification` tinyint(1) NULL DEFAULT NULL COMMENT '规格是否默认选择  0否  1是',
+  `promotion_words` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '促销语',
+  `purchase_link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '采购连接',
+  `purchase_cost` bigint(20) NULL DEFAULT NULL COMMENT '采购成本  单位：分',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `deleted_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for mall_order
+-- ----------------------------
+DROP TABLE IF EXISTS `mall_order`;
+CREATE TABLE `mall_order`  (
+  `id` bigint(20) NOT NULL,
+  `order_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '订单编号',
+  `country` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '国家',
+  `customer_id` bigint(20) NULL DEFAULT NULL COMMENT '客户ID',
+  `customer_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户姓名',
+  `customer_phone` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户手机号',
+  `customer_country` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户国家',
+  `delivery` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '配送方式',
+  `amout` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '金额',
+  `amout_currency_symbol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '货币符号',
+  `goods_id` bigint(20) NULL DEFAULT NULL COMMENT '商品ID',
+  `status` int(1) NULL DEFAULT NULL COMMENT '状态',
+  `delivery_company` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '快递公司',
+  `delivery_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '快递单号',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `deleted_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for mall_template
+-- ----------------------------
+DROP TABLE IF EXISTS `mall_template`;
+CREATE TABLE `mall_template`  (
+  `id` bigint(20) NOT NULL,
+  `region` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '地区',
+  `region_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '地区编码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '模板名称',
+  `currency_symbol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '货币符号',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `deleted_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mall_template
+-- ----------------------------
+INSERT INTO `mall_template` VALUES (1, '台湾', 'NT', '台湾模板1', 'NT＄', '2020-04-13 16:02:36', NULL);
+INSERT INTO `mall_template` VALUES (2, '香港', 'HK', '香港模板1', 'HK＄', '2020-04-13 16:03:13', NULL);
+
+-- ----------------------------
+-- Table structure for mall_user
 -- ----------------------------
 DROP TABLE IF EXISTS `mall_user`;
-CREATE TABLE `mall_user` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '姓名',
-  `phone` varchar(11) NOT NULL COMMENT '手机号',
-  `password` varchar(64) NOT NULL COMMENT '密码',
-  `create_time` datetime NOT NULL,
-  `deleted_time` datetime DEFAULT NULL COMMENT '删除时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `mall_user`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态 0正常   1禁用',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `deleted_time` datetime(0) NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of mall_user
--- ----------------------------
+SET FOREIGN_KEY_CHECKS = 1;
